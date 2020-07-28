a147f0f1c7b4db31b8dcc7ab61e73ea0d0cf13a0
Oliver Foster <olivermartinfoster@gmail.com>
2020-07-28 15:10:30 +0100

    Rewrite to es6

--- js/adapt-background-switcher.js
+++ js/adapt-background-switcher.js
@@ -2,145 +2,120 @@ define([
   'core/js/adapt'
 ], function(Adapt) {
 
-  var BackgroundSwitcherView = Backbone.View.extend({
+  class BackgroundSwitcherPageView extends Backbone.View {
 
-    _blockModels: null,
-    $backgroundContainer: null,
-    $backgrounds: null,
-    $blockElements: null,
-    _firstId: null,
-    _activeId: null,
+    initialize() {
+      _.bindAll(this, 'onBlockInview', 'calculateBackground');
+      this.calculateBackground = _.throttle(this.calculateBackground, 50);
+      this.setupEventListeners();
+      this.setupBackgroundContainer();
+    }
 
-    initialize: function() {
-      _.bindAll(this, 'onBlockInview');
-
-      this.disableSmoothScrolling();
-
-      this._blockModels = _.filter(this.model.findDescendantModels('blocks'), function(model) {
-        return model.get('_backgroundSwitcher');
-      });
-
-      if (this._blockModels.length === 0) {
-        this.onRemove();
-        return;
-      }
-
+    setupEventListeners() {
       this.listenTo(Adapt, {
         'pageView:ready': this.onPageReady,
         remove: this.onRemove
       });
-      this.setupBackgroundContainer();
-    },
+    }
 
-    onPageReady: function() {
-      this.$blockElements = {};
-      this.$backgrounds = {};
+    setupBackgroundContainer() {
+      this.$container = $('<div class="background-switcher__container"></div>');
+      $('body')
+        .addClass('background-switcher-active')
+        .prepend(this.$container);
+    }
 
-      this._blockModels.forEach(function(blockModel) {
-        var id = blockModel.get('_id');
+    onPageReady() {
+      let firstItem;
+      this._items = {};
+      this.model.findDescendantModels('blocks').forEach(blockModel => {
+        const config = blockModel.get('_backgroundSwitcher');
+        if (!config) return;
+        const id = blockModel.get('_id');
+        const $block = this.$el.find(`.${id}`);
+        const $background = $(`<div class="background-switcher__item" style="background-image: url(${config.src});"></div>`);
+        $block
+          .attr('data-backgroundswitcher', id)
+          .addClass('has-background-switcher-image')
+          .on('onscreen.background-switcher', this.onBlockInview);
+        // $block.find('.block__inner').addClass('background-switcher-block-mobile').css({'background-image': `url(${config.mobileSrc})`});
+        this.$container.prepend($background);
+        this._items[id] = {
+          id,
+          $block,
+          $background,
+          measurements: null,
+        };
+        if (firstItem) return;
+        firstItem = this._items[id];
+      });
+      this.showBackground(firstItem);
+    }
 
-        if (!this._firstId) this._firstId = id;
+    onBlockInview(event, measurements) {
+      const id = $(event.target).data('backgroundswitcher');
+      this._items[id].measurements = measurements;
+      this.calculateBackground();
+    }
 
-        var $blockElement = this.$el.find('.'+ id);
+    calculateBackground() {
+      const activeItem = Object.values(this._items).reduce((highest, item) => {
+        const isValid = (item.measurements && item.measurements.onscreen && item.measurements.percentFromTop < 60);
+        if (isValid) return item;
+        return highest;
+      }, null);
+      this.showBackground(activeItem);
+    }
 
-        $blockElement.attr('data-backgroundswitcher', id).addClass('has-background-switcher-image');
-        $blockElement.on('onscreen.background-switcher', this.onBlockInview);
+    showBackground(activeItem) {
+      if (!activeItem) return;
+      const hasChanged = (!this.activeItem || this.activeItem.id !== activeItem.id);
+      if (!hasChanged) return;
+      this.activeItem = activeItem;
+      $('.background-switcher__item.is-active').removeClass('is-active');
+      this.activeItem.$background.addClass('is-active');
+    }
 
-        var options = blockModel.get('_backgroundSwitcher');
-
-        var $backGround = $('<div class="background-switcher__item" style="background-image: url('+ options.src +');"></div>');
-        this.$backgroundContainer.prepend($backGround);
-        this.$backgrounds[id] = $backGround;
-
-        // $blockElement.find('.block__inner').addClass('background-switcher-block-mobile').css({'background-image': 'url('+ options.mobileSrc +')'});
-        this.$blockElements[id] = $blockElement;
-      }.bind(this));
-
-      this._activeId = this._firstId;
-
-      this.showBackground();
-    },
-
-    setupBackgroundContainer : function() {
-      this.$backgroundContainer = $('<div class="background-switcher__container"></div>');
-      $('body').addClass('background-switcher-active');
-      $('body').prepend(this.$backgroundContainer);
-    },
-
-    /**
-     * Turn off smooth scrolling in IE and Edge to stop the background from flickering on scroll
-     */
-    disableSmoothScrolling: function() {
-      if(navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge/)) {
-        $('body').on("mousewheel", function (event) {
-          event.preventDefault();
-          var wd = event.deltaY * event.deltaFactor;
-          var csp = window.pageYOffset;
-          window.scrollTo(0, csp - wd);
-        });
-      }
-    },
-
-    onBlockInview: function(event, measurements) {
-      var isOnscreen = measurements.percentFromTop < 80 && measurements.percentFromBottom < 80;
-      if (!isOnscreen) return;
-
-      var $target = $(event.target);
-      var id = $target.attr('data-backgroundswitcher');
-
-      if (this._activeId === id) return;
-
-      this._activeId = id;
-
-      this.showBackground();
-    },
-
-    showBackground: function() {
-      if (Modernizr.csstransitions){
-        $('.background-switcher__item.is-active').removeClass('is-active');
-        this.$backgrounds[this._activeId].addClass('is-active');
-      }
-      else {
-        $('.background-switcher__item.is-active').animate({ opacity: 0 }, 500, function(){ $(this).removeClass('is-active'); });
-        this.$backgrounds[this._activeId].animate({ opacity: 1 }, 500, function(){ $(this).addClass('is-active'); });
-      }
-    },
-
-    onRemove: function () {
-      for (var id in this.$blockElements) {
-        this.$blockElements[id].off('onscreen.background-switcher');
-      }
-      this.$blockElements = null;
-      this.$backgroundContainer = null;
-      this.$backgrounds = null;
-      this._blockModels = null;
-
+    onRemove() {
+      Object.values(this._items).forEach(item => {
+        item.$block.off('onscreen.background-switcher');
+      });
+      this._items = null;
       this.remove();
-    },
-
-    remove: function() {
-      Adapt.trigger('plugin:beginWait');
-
-      _.defer(_.bind(function() {
-        this.$el.off('onscreen.adaptView');
-        this._isRemoved = true;
-        this.model.setOnChildren('_isReady', false);
-        this.model.set('_isReady', false);
-        $('.background-switcher__container').remove();
-        this.stopListening();
-        Adapt.trigger('plugin:endWait');
-      }, this));
-
-      return this;
     }
 
+    remove() {
+      Adapt.wait.for(done => {
+        _.defer(() => {
+          $('.background-switcher__container').remove();
+          this.stopListening();
+          done();
+        });
+      });
+    }
+
+  }
+
+  Adapt.on('pageView:postRender', view => {
+    const model = view.model;
+    const config = model.get('_backgroundSwitcher');
+    if (!config || !config._isEnabled) return;
+    new BackgroundSwitcherPageView({
+      model,
+      el: view.el
+    });
   });
 
-  Adapt.on('pageView:postRender', function(view) {
-    var model = view.model;
-    if (model.get('_backgroundSwitcher') && model.get('_backgroundSwitcher')._isEnabled) {
-      new BackgroundSwitcherView({model: model, el: view.el });
-    }
-  });
+  /**
+   * Turn off smooth scrolling in IE and Edge to stop the background from flickering on scroll
+   */
+  if (navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge/)) {
+    $('body').on("mousewheel", event => {
+      event.preventDefault();
+      const wd = event.deltaY * event.deltaFactor;
+      const csp = window.pageYOffset;
+      window.scrollTo(0, csp - wd);
+    });
+  }
 
 });
